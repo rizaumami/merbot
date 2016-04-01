aade1218b7fcfa7e4d93431e12bd3751aa9d52a0
Sahri Riza Umami <riza@openmailbox.org>
2016-04-01 20:21:03 +0700

    Google searches by reply.

--- plugins/google.lua
+++ plugins/google.lua
@@ -1,11 +1,31 @@
 do
 
+  local function lmgtfy(url)
+    -- Do the request
+    local res, code = https.request(url)
+    if code ~=200 then return nil  end
+    local data = json:decode(res)
+
+    local stringresults=''
+    for key,result in ipairs(data.responseData.results) do
+      stringresults = stringresults..'<b>'..key..'</b>. '
+                      ..'<a href="'..(result.unescapedUrl or result.url)..'">'
+                      ..unescape_html(result.titleNoFormatting)..'</a>\n'
+    end
+    send_api_msg(msg, receiver, stringresults, true, 'html')
+  end
+
+  local function lmgtfy_by_reply(extra, success, result)
+    local terms = result.text
+    local url = extra.url..'&q='..(URL.escape(terms) or '')
+    lmgtfy(url)
+  end
+
   local function run(msg, matches)
     -- comment this line if you want this plugin to works in private message.
     if not is_chat_msg(msg) and not is_admin(msg.from.peer_id) then return nil end
 
     local url        = 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0'
-    local parameters = '&q='..(URL.escape(matches[2]) or '')
 
     if is_chat_msg(msg) then
       url = url..'&rsz=5'
@@ -23,18 +43,12 @@ do
       url = url .. '&safe=active'
     end
 
-    -- Do the request
-    local res, code = https.request(url..parameters)
-    if code ~=200 then return nil  end
-    local data = json:decode(res)
-
-    local stringresults=''
-    for key,result in ipairs(data.responseData.results) do
-      stringresults = stringresults..'<b>'..key..'</b>. '
-                      ..'<a href="'..(result.unescapedUrl or result.url)..'">'
-                      ..unescape_html(result.titleNoFormatting)..'</a>\n'
+    if msg.reply_id then
+      get_message(msg.reply_id, lmgtfy_by_reply, {url=url})
+    else
+      local url = url..'&q='..(URL.escape(matches[2]) or '')
+      lmgtfy(url)
     end
-    send_api_msg(msg, receiver, stringresults, true, 'html')
   end
 
 --------------------------------------------------------------------------------
@@ -45,15 +59,22 @@ do
       '<code>!google [terms]</code>',
       '<code>!g [terms]</code>',
       'Safe searches Google',
+      '<code>!google</code>',
+      '<code>!g</code>',
+      'Safe searches Google by reply. The search terms is the replied message text.',
       '<code>!googlensfw [terms]</code>',
       '<code>!gnsfw [terms]</code>',
       'Searches Google (include NSFW)'
+      '<code>!googlensfw</code>',
+      '<code>!gnsfw</code>',
+      'Searches Google (include NSFW). The search terms is the replied message text.'
     },
     patterns = {
-      '^!(g) (.*)$',
-      '^!g(nsfw) (.*)$',
-      '^!(google) (.*)$',
-      '^!google(nsfw) (.*)$',
+      '^!(g)$', '^!(google)$',
+      '^!g(nsfw)$', '^!google(nsfw)$',
+      '^%.([g|G]oogle)$',
+      '^!(g) (.*)$', '^!(google) (.*)$',
+      '^!g(nsfw) (.*)$', '^!google(nsfw) (.*)$',
       '^%.([g|G]oogle) (.*)$'
     },
     run = run
