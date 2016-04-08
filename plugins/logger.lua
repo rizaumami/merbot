afe7b6d42385e6f24b8df58a08a1de8119ba691a
Sahri Riza Umami <riza@openmailbox.org>
2016-04-08 16:30:11 +0700

    Fix logger.lua

--- plugins/logger.lua
+++ plugins/logger.lua
@@ -9,7 +9,7 @@ if the log file size is big.
 
 do
 
-  function run(msg, matches)
+  local function pre_process(msg)
     local gid = tonumber(msg.to.peer_id)
     if _config.administration[gid] and is_chat_msg(msg) then
       local message = serpent.dump(msg, {comment=false})
@@ -19,12 +19,26 @@ do
       logfile:write(message..'\n')
       logfile:close()
     end
-    if is_mod(msg, gid, msg.from.peer_id) then
+    return msg
+  end
+
+  local function run(msg, matches)
+    local uid = msg.from.peer_id
+    local loggid = msg.to.peer_id
+    local receiver = get_receiver(msg)
+
+    if matches[2] and matches[2]:match('^%d+$') then
+      loggid = matches[2]
+    end
+
+    if is_owner(msg, loggid, uid) then
       if matches[1] == 'get' then
-        send_document('chat#id'..gid, './data/'..gid..'/'..gid..'.log', ok_cb, false)
+        send_document(receiver, './data/'..loggid..'/'..loggid..'.log', ok_cb, false)
       elseif matches[1] == 'pm' then
-        send_document('user#id'..msg.from.peer_id, './data/'..gid..'/'..gid..'.log', ok_cb, false)
+        send_document('user#id'..uid, './data/'..loggid..'/'..loggid..'.log', ok_cb, false)
       end
+    else
+      reply_msg(msg.id, 'You have no privilege to get '..loggid..' log.', ok_cb, true)
     end
   end
 
@@ -33,7 +47,13 @@ do
   return {
     description = 'Logging group messages.',
     usage = {
-      moderator = {
+      sudo = {
+        '<code>!log get [chat_id]</code>',
+        'Send <code>chat_id</code> chat log.',
+        '<code>!log pm [chat_id]</code>',
+        'Send <code>chat_id</code> chat log to private message'
+      },
+      owner = {
         '<code>!log get</code>',
         'Send chat log to its chat group',
         '<code>!log pm</code>',
@@ -41,9 +61,11 @@ do
       },
     },
     patterns = {
-      '^!log (.+)$'
+      '^!log (%a+)$',
+      '^!log (%a+) (%d+)$'
     },
-    run = run
+    run = run,
+    pre_process = pre_process,
   }
 
 end
