20488b1ff652009d14b57ba0341c631553e25b34
Sahri Riza Umami <riza@openmailbox.org>
2016-04-18 15:43:43 +0700

    Change whois web service.

--- plugins/whois.lua
+++ plugins/whois.lua
@@ -1,24 +1,12 @@
 do
 
   local function run(msg, matches)
-    local tgexec = "./tg/bin/telegram-cli -c ./data/tg-cli.config -p default -De "
-    local url = 'http://api.hackertarget.com/whois/?q='..URL.escape(matches[1])
-    local whois_file = matches[1]:gsub('%.', '-')
-    local whois_file = '/tmp/'..whois_file
-    local w_file = ltn12.sink.file(io.open(whois_file, 'w'))
-    http.request {
-        url = url,
-        sink = w_file,
-      }
-
-    local file = io.open(whois_file, 'r')
-    local file = file:read('*all')
-    if file:match('DOMAIN NOT FOUND') or file:match('No match') then
-      reply_msg(msg.id, 'Domain not found.', ok_cb, true)
-    else
-      os.execute(tgexec.."\'reply_file "..msg.id.." "..whois_file.."\'")
-      --send_document(get_receiver(msg), whois_file, rmtmp_cb, {file_path=whois_file})
-    end
+    local domain = URL.escape(matches[1])
+    local url = 'http://www.whoisxmlapi.com/whoisserver/WhoisService?domainName='..domain..'&outputFormat=JSON'
+    local res, code = http.request(url)
+    local data = json:decode(res)
+    local whois = data.WhoisRecord.registryData.rawText
+    reply_msg(msg.id, whois, ok_cb, true)
   end
 
   return {
