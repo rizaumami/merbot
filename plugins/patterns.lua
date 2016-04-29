5f590bece05cc4e001877aef367b11b59b7fbf46
Sahri Riza Umami <riza@openmailbox.org>
2016-04-29 22:24:37 +0700

    Use otouto's patterns. Strikethrough patterns.lua varies in results because of differences in font set in various telegram client.

--- plugins/patterns.lua
+++ plugins/patterns.lua
@@ -1,28 +1,18 @@
 do
 
-  local function strikethrough(text)
-    local striked = {}
-    for i = 1, #text do
-      local output = text:sub(i, i)
-      striked[i] = output..'̶'
-    end
-    return table.concat(striked)
-  end
-
   local function action_by_reply(extra, success, result)
     local output = result.text or ''
     local m1, m2 = extra.text:match('^/?s/(.-)/(.-)/?$')
     if not m2 then
       return
     end
-    local word1 = strikethrough(m1)
-    output = output:gsub(m1, word1..m2)
-    output = output:sub(1, 4000)
+    output = output:gsub(m1, m2)
+    output = 'Did you mean:\n"' .. output:sub(1, 4000) .. '"'
     reply_msg(result.id, output, ok_cb, true)
   end
 
   local function run(msg, matches)
-    if msg.reply_id and matches[1]:match('^/?s/(.-)/(.-)/?$') then
+    if msg.reply_id then
       get_message(msg.reply_id, action_by_reply, msg)
     end
   end
