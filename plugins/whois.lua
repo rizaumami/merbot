db1af24a300bcd999975e519560f55ab05e81589
Sahri Riza Umami <riza@openmailbox.org>
2016-05-01 22:37:23 +0700

    Add options; whois as text, send in pm and send text file into pm (commented).

--- plugins/whois.lua
+++ plugins/whois.lua
@@ -1,12 +1,39 @@
 do
 
-  local function run(msg, matches)
-    local domain = URL.escape(matches[1])
+  local whofile = '/tmp/whois.txt'
+
+  local function get_whois(domain)
+    local domain = URL.escape(domain)
     local url = 'http://www.whoisxmlapi.com/whoisserver/WhoisService?domainName='..domain..'&outputFormat=JSON'
     local res, code = http.request(url)
     local data = json:decode(res)
+    vardump(data)
     local whois = data.WhoisRecord.registryData.rawText
-    reply_msg(msg.id, whois, ok_cb, true)
+
+    local file = io.open(whofile, 'w')
+    file:write(whois)
+    file:flush()
+    file:close()
+
+    return whois
+  end
+
+  local function run(msg, matches)
+    vardump(matches)
+    local whoinfo = get_whois(matches[1])
+    if matches[2] then
+      if matches[2] == 'txt' then
+        reply_file(msg.id, whofile, ok_cb, true)
+      end
+      if matches[2] == 'pm' then
+        send_api_msg(msg, msg.from.peer_id, whoinfo, true, '')
+      end
+      -- if matches[2] == 'pmtxt' then
+      --   send_document('user#id'..msg.from.peer_id, whofile, rmtmp_cb, {file_path=whofile})
+      -- end
+    else
+      reply_msg(msg.id, whoinfo, ok_cb, true)
+    end
   end
 
   return {
@@ -14,9 +41,21 @@ do
     usage = {
       '<code>!whois [url]</code>',
       'Returns whois lookup for <code>[url]</code>',
+      '',
+      '<code>!whois [url] txt</code>',
+      'Returns whois lookup for <code>[url]</code> and then send as text file.',
+      '',
+      '<code>!whois [url] pm</code>',
+      'Returns whois lookup for <code>[url]</code> into requester PM.',
+      '',
+      -- '<code>!whois [url] pmtxt</code>',
+      -- 'Returns whois lookup file for <code>[url]</code> and then send into requester PM.',
     },
     patterns = {
-      '^!whois (.*)$'
+      '^!whois (%g+)$',
+      '^!whois (%g+) (txt)$',
+      '^!whois (%g+) (pm)$',
+      -- '^!whois (%g+) (pmtxt)$'
     },
     run = run
   }
